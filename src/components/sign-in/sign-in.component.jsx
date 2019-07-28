496b9b04fd2d5ffe00827d22a83b1c2ec249703d
Chomo Gwom <chomogwom@gmail.com>
2019-07-28 17:54:42 +0200

    added sign in form and styling

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+
+    handleSubmit =(e)=>{
+        e.preventDefautlt();
+
+        this.setState({
+            email: '',
+            passoword: ''
+        })
+
+    }
+
+    handleChange =(e)=>{
+        const {value, name} = e.target;
+
+        this.setState({
+            [name]: value,
+
+        })
+    }
+
+    render(){
+        return (
+            <div className ="sign-in">
+                <h2>I already have an Account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit ={this.handleSubmit}>
+                    <FormInput 
+                        name ="email"
+                        type ="email" 
+                        value = {this.state.email} 
+                        handleChange ={this.handleChange}   
+                        label ="email"
+                    required />
+                    {/* <label>Email</label> */}
+
+                    <FormInput 
+                        name ="password" 
+                        type ="password" 
+                        value = {this.state.password}  
+                        handleChange ={this.handleChange}  
+                        label = "password"
+                    required />
+                    {/* <label>Passowrd</label>    */}
+
+                    <input type ="submit" value = "Submit form"  />
+
+                </form>
+            </div> 
+        );
+    }
+}
+
+
+export default SignIn;
