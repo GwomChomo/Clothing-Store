d1e64665cd27ec95cd93ea6337a215afb7421ca2
Chomo Gwom <chomogwom@gmail.com>
2019-07-29 14:06:41 +0200

    Added redirect to sign in route and refactored action creators

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
+//components and pages
 import Header from './components/header/header.component';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+
+//firebase stuff
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+
+//redux stuff
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions.js';
 
@@ -34,8 +38,10 @@ class  App extends React.Component {
           })
         })
       }
-      
+      else{
         setCurrentUser(userAuth);
+      }
+        
     })
   }
 
@@ -44,13 +50,14 @@ class  App extends React.Component {
   }
 
   render(){
+    
     return (
       <div>
         <Header />
         <Switch>
           <Route  exact path="/" component ={HomePage}/>
           <Route path="/shop" component ={ShopPage} />
-          <Route path="/signin" component ={SignInAndSignUpPage} />
+          <Route exact path="/signin" render = {()=>this.props.currentUser? (<Redirect to='/' />): (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
@@ -58,8 +65,12 @@ class  App extends React.Component {
   
 }
 
+const mapStateToProps =({user})=>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps=dispatch=>({
   setCurrentUser: user=>dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
+//firebase utils
 import {auth} from '../../firebase/firebase.utils';
+
+//Redux utils
+import {connect} from 'react-redux';
 
 const Header =({currentUser})=>(
     <div className ="header">
         <Link to ='/' className ="logo-container" >
             <Logo className= "logo" />
         </Link>
-
         <div className ="options">
             <Link to ="/shop" className ="option">Shop</Link>
             <Link to ="/shop" className ="option">Contact</Link>
@@ -31,5 +33,5 @@ const mapStateToProps = state =>({
     currentUser: state.user.currentUser
 })
 
-//connect is a Higher Order Component
+//connect is a Higher Order Component that generates container components. It maps the state of the store as well as the dispatches to the store, to the props of the containers it generates.
 export default connect(mapStateToProps)(Header);
