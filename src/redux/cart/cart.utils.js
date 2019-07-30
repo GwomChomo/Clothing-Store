587803ed6199794b0abefafb0aab1dcbbb7f22eb
Chomo Gwom <chomogwom@gmail.com>
2019-07-30 16:38:53 +0200

    Added increment/decremenet feature to checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,17 +1,24 @@
 export const addItemToCart =(cartItems, itemToAdd)=>{
     const existingItem = cartItems.find(cartItem =>cartItem.id === itemToAdd.id);
     
-    
     if(existingItem){
         return cartItems.map(cartItem => cartItem.id === itemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1}
             :cartItem
         )
     }
-
     return [...cartItems, {...itemToAdd, quantity: 1}]
 }
 
 export const clearItemFromCart = (cartItems, itemToClear) =>{
     return cartItems.filter(cartItem => cartItem.id !== itemToClear.id)
+}
+
+export const removeItem =(cartItems, itemToRemove) =>{
+    const existingItem = cartItems.find(cartItem =>cartItem.id === itemToRemove.id);
+
+    if(existingItem.quantity === 1){
+        return clearItemFromCart(cartItems, itemToRemove)
+    }
+    return cartItems.map(cartItem => cartItem.id === itemToRemove.id? {...cartItem, quantity: cartItem.quantity - 1}: cartItem)
 }
 
